42c74e814553ab0601f0a446fc4ee60c9cb4486e
Aditya Sharma <adityasharma4510@gmail.com>
2025-08-21 02:55:19 +0530

    First commit to next js

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+         className="geist-sans geist-mono antialiased"
+
       >
         {children}
       </body>
